ded927af30f14e36cae026b0b4973d82681901a3
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-05 14:38:53 +0200

    49. Operadores: Ternário

---
+++ exercicios-js/fundamentos/ternario.js
@@ -0,0 +1,12 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' //operador ternario
+
+/** TERNARIO
+ * em primeiro é uma expressão que da sempre true o r false
+ * depois tem a ?
+ * na primeira é a resposta do true
+ * na segunda é a resposta do false
+ */
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
